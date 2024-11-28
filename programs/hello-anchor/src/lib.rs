ae1d7b3cc6e794583e534ef2883f0035d49a9b24
ntnobara <nguyentoan231196@gmail.com>
2024-11-28 20:16:14 +0700

    update the lib.rs in src

--- programs/hello-anchor/src/lib.rs
+++ programs/hello-anchor/src/lib.rs
@@ -1,33 +1,24 @@
 use anchor_lang::prelude::*;
 
-// This is your program's public key and it will update
-// automatically when you build the project.
-declare_id!("11111111111111111111111111111111");
+declare_id!("");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 #[program]
-mod hello_anchor {
+pub mod favories {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
-        ctx.accounts.new_account.data = data;
-        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
-        Ok(())
-    }
-}
 
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    // We must specify the space in order to initialize an account.
-    // First 8 bytes are default account discriminator,
-    // next 8 bytes come from NewAccount.data being type u64.
-    // (u64 = 64 bits unsigned integer = 8 bytes)
-    #[account(init, payer = signer, space = 8 + 8)]
-    pub new_account: Account<'info, NewAccount>,
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub fn set_favorites() -> Return<()> {}
 }
 
 #[account]
-pub struct NewAccount {
-    data: u64
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
 }
